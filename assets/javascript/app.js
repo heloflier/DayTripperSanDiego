27d224f6b110695c48a1c5f73c290a7c961190ba
Nate <bhhuynh@ucsd.edu>
2017-05-27 14:41:51 -0700

    add in p.o.i in div

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,16 +11,20 @@ var embedAPIkey = "AIzaSyADcGH--swBBpg6-7tYcluAngele15Sz6M";
 var userinput   = "hungtington beach";
 $("#map1").attr("src","https://www.google.com/maps/embed/v1/place?key="+embedAPIkey+"&q="+userinput);
 
-var lat = "";
-var long = "";
-//Google long. lat. coordinates
+var lat;
+var long;
+//Google long. lat. coordinates GEOCODING
 var coordinatequeryURL = "https://maps.googleapis.com/maps/api/geocode/json?address="+userinput+"&key="+embedAPIkey;
 var nearbyqueryURL = "";
 var map;
+
+//We need initMap for google library to work, doesn't necessarily need to define just declaration
 function initMap() {
 	// body...
 }
 
+
+//This AJAX call the google geocoding API for the user's long/lat and find places/weather near that coordinates
 $.ajax({
           url: coordinatequeryURL,
           method: "GET"
@@ -43,18 +47,22 @@ $.ajax({
     	  name: "asian"
 		  }, callback);
 		}
-
-		
 		initMap();
+
 })
 
 //Google nearby Places
 
 function callback(results, status) {
 	if (status === google.maps.places.PlacesServiceStatus.OK) {
-	console.log(results);	
-	console.log(results[0].name);
-	console.log(results[1].name);
+		console.log(results);	
+		console.log(results[0].name);
+		console.log(results[1].name);
+
+		for(var i=0; i<results.length;i++){
+		$(".mappanel").append("<h3>"+results[i].name+"</h3>");
+		$(".mappanel").append("Rating: "+results[i].rating);
+		}
 	}
 }
 
